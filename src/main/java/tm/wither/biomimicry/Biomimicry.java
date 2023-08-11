65e1ccbbcf4e1f1be347825347b67edb203d427b
WitherTM <101216712+WitherTM@users.noreply.github.com>
2023-08-11 21:23:12 +0200

    examplemod deletion

--- src/main/java/tm/wither/biomimicry/Biomimicry.java
+++ src/main/java/tm/wither/biomimicry/Biomimicry.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package tm.wither.biomimicry;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
@@ -30,8 +30,8 @@ import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
+@Mod(Biomimicry.MODID)
+public class Biomimicry
 {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "examplemod";
@@ -61,7 +61,7 @@ public class ExampleMod
                 output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
             }).build());
 
-    public ExampleMod()
+    public Biomimicry()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
