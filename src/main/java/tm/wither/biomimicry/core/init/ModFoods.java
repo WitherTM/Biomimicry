a46dc295fcd94a78dd54dd629a4a87bb6b4ec9c6
WitherTM <101216712+WitherTM@users.noreply.github.com>
2023-10-17 19:59:30 +0200

    optimizations

--- src/main/java/tm/wither/biomimicry/core/init/ModFoods.java
+++ src/main/java/tm/wither/biomimicry/core/init/ModFoods.java
@@ -3,5 +3,8 @@ package tm.wither.biomimicry.core.init;
 import net.minecraft.world.food.FoodProperties;
 
 public class ModFoods {
-    public static final FoodProperties CHOPPED_CARROTS = new FoodProperties.Builder().alwaysEat().nutrition(1).fast().build();
+    public static final FoodProperties CHOPPED_CARROTS = new FoodProperties.Builder().alwaysEat().nutrition(2).fast().build();
+    public static final FoodProperties SWEET_BERRY = new FoodProperties.Builder().fast().nutrition(1).build();
+    public static final FoodProperties MEAT_PASTE = new FoodProperties.Builder().nutrition(3).build();
+
 }
