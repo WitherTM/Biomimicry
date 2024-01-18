6bfb8b00b09feb2fb34344992936925dd3242bad
truemudkip <truemudkip123@gmail.com>
2024-01-18 21:03:17 +0200

    Sac test lol

--- src/main/java/tm/wither/biomimicry/core/init/ModItems.java
+++ src/main/java/tm/wither/biomimicry/core/init/ModItems.java
@@ -43,6 +43,8 @@ public class ModItems {
             () -> new Item(new Item.Properties().food(ModFoods.MEATBALL )));
     public static final RegistryObject<Item> WHEAT_RICE_MIX = ITEMS.register("wheat_rice_mix",
             () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> EMPTY_SAC = ITEMS.register("empty_sac",
+            () -> new Item(new Item.Properties()));
 
 
     public static final RegistryObject<Item> CACTUS_SPEARHEAD = ITEMS.register("cactus_spearhead",
